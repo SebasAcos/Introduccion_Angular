704bafcc7fa06cad1515effac0bf164cf3214e66
Sebastian Acosta <sebastian.acosta@grantitan.com>
2022-01-27 09:52:07 -0500

    cambios de los posters de las peliculas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,17 +16,20 @@ export class AppComponent implements OnInit{
       {
     titulo: 'spiderman',
     fecha: new Date(),
-    presio: 3000
+    presio: 3000,
+    poster:'https://www.xtrafondos.com/thumbs/vertical/1_3452.jpg'
   },
   {
     titulo: 'IronMan',
     fecha: new Date('03-02-2022'),
-    presio: 3400
+    presio: 3400,
+    poster:'https://www.xtrafondos.com/thumbs/vertical/1_9224.jpg'
   },
   {
-    titulo: 'UFC',
+    titulo: 'WWE',
     fecha: new Date('06-03-2020'),
-    presio: 5000
+    presio: 5000,
+    poster: 'https://www.xtrafondos.com/thumbs/vertical/1_796.jpg'
   }
    ]
 }, 4000);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from './material/material.module';
 import { MenuComponent } from './menu/menu.component';
 import { RatingComponent } from './utilidades/rating/rating.component';
-import { CicloDeVidaComponent } from './ciclo-de-vida/ciclo-de-vida.component'
+
 @NgModule({
   declarations: [
     AppComponent,
     ListadoPeliculasComponent,
     ListadoGenericoComponent,
     MenuComponent,
-    RatingComponent,
-    CicloDeVidaComponent
+    RatingComponent
+
   ],
   imports: [
     BrowserModule,
